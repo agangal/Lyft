29a324c63e2d762152c06861b27a823f9af83179
agangal <ashish.gangal1990@gmail.com>
2016-12-21 05:28:03 -0800

    Complete 3-legged sign in

--- LyftUWP/Helpers/AuthHelper.cs
+++ LyftUWP/Helpers/AuthHelper.cs
@@ -7,11 +7,14 @@ using Windows.Security.Authentication.Web;
 
 namespace LyftUWP.Helpers
 {
+    using Windows.Web.Http;
+    using Windows.Web.Http.Headers;
+
     public class AuthHelper
     {
         public static async Task<bool> GetAuthorizationCode()
         {
-            string authurl = "https://api.lyft.com/oauth/authorize?client_id=" + Settings.CLIENT_ID + "&response_type=code&scope=public&state=" + Settings.STATE;
+            string authurl = Settings.AUTHORIZATION_CODE_URI + "?client_id=" + Settings.CLIENT_ID + "&response_type=code&scope=public&state=" + Settings.STATE;
             Uri StartUri = new Uri(authurl);
             Uri EndUri = new Uri("https://ashishgangal.com/");
             WebAuthenticationResult WebAuthResult = await WebAuthenticationBroker.AuthenticateAsync(WebAuthenticationOptions.None, StartUri, EndUri);
@@ -21,10 +24,41 @@ namespace LyftUWP.Helpers
                 string substring = responsedata.Substring(responsedata.IndexOf("code"));
                 string[] keyValuePairs = substring.Split('&');
                 string[] splits = keyValuePairs[0].Split('=');
-                string code = splits[1];
-                System.Diagnostics.Debug.WriteLine(code);
+                Settings.AUTHORIZATION_CODE = splits[1];
+                System.Diagnostics.Debug.WriteLine(Settings.AUTHORIZATION_CODE);
             }
             return true;
+        }
+
+        public static async Task<bool> GetAccessToken()
+        {
+            HttpClient httpClient = new HttpClient();
+            try
+            {
+                string content = "client_id="+ Settings.CLIENT_ID + "&client_secret=" + Settings.CLIENT_SECRET + "&redirect_uri=https%3A%2F%2Fashishgangal.com%2F&grant_type=authorization_code&state="+ Settings.STATE + "&code=" + Settings.AUTHORIZATION_CODE;
+                HttpStringContent stringcont = new HttpStringContent(content);                
+                var bytearray = Encoding.ASCII.GetBytes(Settings.CLIENT_ID + ":"+ Settings.CLIENT_SECRET);
+                httpClient.DefaultRequestHeaders.TryAppendWithoutValidation("Authorization", "Basic " + Convert.ToBase64String(bytearray));
+                stringcont.Headers.ContentType = new HttpMediaTypeHeaderValue("application/x-www-form-urlencoded");
+                var httpresponseMessage = await httpClient.PostAsync(new Uri(Settings.ACCESS_TOKEN_URI), stringcont);
+                string resp = await httpresponseMessage.Content.ReadAsStringAsync();
+            }
+            catch (Exception ex)
+            {
+
+            }
+            return true;
+        }
+
+        public static async Task<bool> UserSignIn()
+        {
+            bool code_result = await GetAuthorizationCode();
+            if (code_result == true)
+            {
+                bool token_result = await GetAccessToken();
+                return token_result;
+            }
+            return false;
         }
     }
 }
