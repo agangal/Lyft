c41215008fabf2009a080f5e26c13ecf14096732
agangal <ashish.gangal1990@gmail.com>
2016-12-21 21:15:33 -0800

    Adding Ride type and ETA URIs

--- LyftUWP/Helpers/URIHelper.cs
+++ LyftUWP/Helpers/URIHelper.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.Storage;
 
 namespace LyftUWP.Helpers
 {
@@ -16,6 +17,28 @@ namespace LyftUWP.Helpers
         public static string AUTHORIZATION_CODE_URI
         {
             get { return "https://api.lyft.com/oauth/authorize"; }
+        }
+
+        public static string RIDE_TYPE_URI
+        {
+            get { return "https://api.lyft.com/v1/ridetypes"; }
+        }
+
+        public static string RIDE_TYPE_URI_LASTUSED
+        {
+            get { return ApplicationData.Current.LocalSettings.Values["RIDE_TYPE_URI_LASTUSED"].ToString(); }
+            set { ApplicationData.Current.LocalSettings.Values["RIDE_TYPE_URI_LASTUSED"] = value; }
+        }
+
+        public static string ETA_URI
+        {
+            get { return "https://api.lyft.com/v1/eta"; }
+        }
+
+        public static string ETA_URI_LASTUSED
+        {
+            get { return ApplicationData.Current.LocalSettings.Values["ETA_URI_LASTUSED"].ToString(); }
+            set { ApplicationData.Current.LocalSettings.Values["ETA_URI_LASTUSED"] = value; }
         }
     }
 }
