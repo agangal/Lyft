399bc8057c7149d1921713894f1e575532bc3863
agangal <ashish.gangal1990@gmail.com>
2016-12-22 05:16:03 -0800

    get user location and call ride type and eta api

--- LyftUWP/Pages/RidesPage.xaml.cs
+++ LyftUWP/Pages/RidesPage.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
 using Windows.Devices.Geolocation;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
@@ -18,6 +19,7 @@ using Windows.UI.Xaml.Navigation;
 
 namespace LyftUWP.Pages
 {
+    using LyftUWP.Helpers;
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
@@ -29,10 +31,29 @@ namespace LyftUWP.Pages
             this.InitializeComponent();
         }
 
-        protected override void OnNavigatedTo(NavigationEventArgs e)
+        protected override async void OnNavigatedTo(NavigationEventArgs e)
         {
             base.OnNavigatedTo(e);
-            GetUserLocation();
+            UpdateRideTypes();
+        }
+
+        private async void UpdateRideTypes()
+        {
+            Geoposition pos = await GetUserLocation();
+            if (pos == null)
+            {
+
+            }
+            else
+            {
+                // Get type of rides available
+                //string ride_type = await URIHelper.GetRequest(URIHelper.RIDE_TYPE_URI + "?lat=" + pos.Coordinate.Point.Position.Latitude.ToString() + "&lng=" + pos.Coordinate.Point.Position.Longitude.ToString());
+                //string eta = await URIHelper.GetRequest(URIHelper.ETA_URI + "?lat=" + pos.Coordinate.Point.Position.ToString() + "&lng=" + pos.Coordinate.Point.Position.Longitude.ToString());
+                string ride_type = await URIHelper.GetRequest(URIHelper.RIDE_TYPE_URI + "?lat=47.61" + "&lng=-122.33");
+                string eta = await URIHelper.GetRequest(URIHelper.ETA_URI + "?lat=47.61" + "&lng=-122.33");
+
+                // Get ETA for each
+            }
         }
 
         protected override void OnNavigatedFrom(NavigationEventArgs e)
@@ -41,15 +62,18 @@ namespace LyftUWP.Pages
             _geolocator.PositionChanged -= _geolocator_PositionChanged;
         }
 
-        private async void GetUserLocation()
+        private async Task<Geoposition> GetUserLocation()
         {
             var accessStatus = await Geolocator.RequestAccessAsync();
             if (accessStatus == GeolocationAccessStatus.Allowed)
             {
                 _geolocator = new Geolocator { ReportInterval = 5000, DesiredAccuracyInMeters = 200 };
                 _geolocator.PositionChanged += _geolocator_PositionChanged;
+                Geoposition pos = await _geolocator.GetGeopositionAsync();
+                return pos;
                 //_geolocator.StatusChanged += _geolocator_StatusChanged;               
             }
+            return null;
         }
 
         private async void _geolocator_PositionChanged(Geolocator sender, PositionChangedEventArgs args)
