ec486685ea0c37f7213610912cc0f912c9d24b7c
agangal <ashish.gangal1990@gmail.com>
2016-12-30 09:56:23 -0800

    looking for headers in incoming POST requests

--- LyftServer/app.js
+++ LyftServer/app.js
@@ -8,7 +8,12 @@ var app = express();
 //module.exports = require('./config/express')(app, config);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
+app.post('/rideshook', function(req,res,next){
   console.log(req.body);
+  console.log(req.headers['content-type']);
+  res.send('ping');
+});
+
 
 app.listen(3000, function () {
   console.log('Express server listening on port 3000');
