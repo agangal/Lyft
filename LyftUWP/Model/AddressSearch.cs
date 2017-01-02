9bf79a42ede325101c7e95bb2381b85c55d257b2
agangal <ashish.gangal1990@gmail.com>
2017-01-01 22:45:15 -0800

    Object to search for an address

---
+++ LyftUWP/Model/AddressSearch.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LyftUWP.Model
+{
+    class AddressSearch
+    {
+        public string formatted_address { get; set; }
+        public double latitude { get; set; }
+        public double longitude { get; set; }
+    }
+}
